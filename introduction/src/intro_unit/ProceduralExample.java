015fb11a39610a390169c7100a5563ef197af4c2
Jia Ming <jma8774@bths.edu>
2016-09-15 13:19:01 -0400

    jillian is in senior year

--- introduction/src/intro_unit/ProceduralExample.java
+++ introduction/src/intro_unit/ProceduralExample.java
@@ -11,5 +11,7 @@ public class ProceduralExample {
 	public static void main(string[]args){
 		String[] names={"Jillian","Joseph","Jordan"};
 		System.out.println("My name is "+names[0]+".");
+		String[] years={"sophomore","junior","senior"};
+		System.out.println("I am a "+years[2]);
 	}
 }
