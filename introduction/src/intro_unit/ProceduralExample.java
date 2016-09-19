53fbb38f28fbdc3dce2956d552d3fe91036afa50
Student 6 <Student 6@10.8.33.164>
2016-09-19 12:53:58 -0400

    fixed some errors

--- introduction/src/intro_unit/ProceduralExample.java
+++ introduction/src/intro_unit/ProceduralExample.java
@@ -8,15 +8,11 @@ package intro_unit;
  *
  */
 public class ProceduralExample {
-	public static void main(string[]args){
+	public static void main(String[]args){
 		String[] names={"Jillian","Joseph","Jordan"};
-<<<<<<< HEAD
 		String[] years={"Senior","Junior","Sophomore"};
 		System.out.println("My name is "+names[0]+" and I am in my "+years[0]+" year.");
-=======
 		System.out.println("My name is "+names[0]+".");
-		String[] years={"sophomore","junior","senior"};
 		System.out.println("I am a "+years[2]);
->>>>>>> branch 'master' of https://github.com/xxspys/classwork.git
 	}
 }
