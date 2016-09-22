dccd17cb47372939709b51ac1f9646768920f8e9
Jimmy <Jimmy@192.168.1.3>
2016-09-21 20:00:11 -0400

    card stack hw

--- introduction/src/mathRandom/DeckOfCards.java
+++ introduction/src/mathRandom/DeckOfCards.java
@@ -6,7 +6,7 @@ public class DeckOfCards {
 		System.out.println(pickOneCard());
 	}
 	public static String pickOneCard(){
-		String[] arr1 = {"1","2","3","4","5","6","7","8","9","10","Jack","Queen","King"};
+		String[] arr1 = {"Ace","2","3","4","5","6","7","8","9","10","Jack","Queen","King"};
 		String[] arr2 = {"Diamonds","Hearts","Clubs","Spades"};
 		String card = "The "+ arr1[(int) (Math.random()*(arr1.length))] + " of " + arr2[(int) (Math.random()*arr2.length)] + ".";
 		return card;
