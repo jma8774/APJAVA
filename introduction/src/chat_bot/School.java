939b55957e76b12e23fe2bd0a91b389ed1685ba7
Student 6 <Student 6@10.8.33.164>
2016-09-23 13:18:31 -0400

    chat bot

--- introduction/src/chat_bot/School.java
+++ introduction/src/chat_bot/School.java
@@ -9,6 +9,10 @@ public class School implements Topic{
 		while(inSchoolLoop){
 			JiaMingMain.print("Tell me about school.");
 			schoolResponse = JiaMingMain.getInput();
+			if(schoolResponse.indexOf("stop")>=0){
+				inSchoolLoop= false;
+				JiaMingMain.talkForever();
+			}
 			JiaMingMain.print("That's my favorite " + "part about school too.");
 		}
 	}
