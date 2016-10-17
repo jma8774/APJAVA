e5f0f4920364665933d08f1c1b3a517578c8b94f
Student 6 <Student 6@10.8.33.164>
2016-10-17 13:16:37 -0400

    array demonstrations

--- introduction/src/array/Demonstration.java
+++ introduction/src/array/Demonstration.java
@@ -10,6 +10,12 @@ public class Demonstration {
 		int[] diceRolls = new int[1000];
 		populateWith2DiceRolls(diceRolls);
 		verifyMathRandom(diceRolls);
+		String[] deckOfCards = new String[52];
+		populateWithDeck(deckOfCards);
+	}
+	private static void populateWithDeck(String[] deckOfCards) {
+		// TODO Auto-generated method stub
+		
 	}
 	private static void populateWith2DiceRolls(int[] arr){
 		for(int i=0;i<arr.length;i++){
@@ -32,7 +38,7 @@ public class Demonstration {
 			possibilities[arr[i]-1]++;
 		}
 		for(int i=0;i<possibilities.length;i++){
-			System.out.println(possibilities[i]);
+			System.out.println(i+1+" appeared "+((double)(possibilities[i])/1000)*100+"% of the time.");
 		}
 	}
 	private static void printArray(int[] arr){
