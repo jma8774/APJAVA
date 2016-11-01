09003e9698a6c91d925dba93eb2e5b92ef9877cd
Jimmy <Jimmy@192.168.1.2>
2016-10-31 21:55:45 -0400

    TESTING 123

---
+++ introduction/src/forTestingRandomStuff/MainClass.java
@@ -0,0 +1,15 @@
+package forTestingRandomStuff;
+
+public class MainClass {
+	private static ClassOne className;
+	private static ClassOne classGrade;
+	private static ClassOne numStudents;
+	
+	public static void main(String[] args) {
+		System.out.println(ClassOne.className);
+		System.out.println(ClassOne.classGrade);
+		ClassOne.classGrade=ClassOne.changeValue(11);
+		System.out.println(ClassOne.classGrade);
+	}
+		
+}
